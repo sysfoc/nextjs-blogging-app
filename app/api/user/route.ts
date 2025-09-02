6a1f5ee71887e2f0b18b9d1032a656fbb6df7d21
hamza-fullstackdev <hamzafullstackdev1@gmail.com>
2025-09-02 10:54:54 +0500

    Fix Deployment Bug

--- app/api/user/route.ts
+++
@@ -1,8 +0,0 @@
-import { connectToDatabase } from "@/app/utils/connection";
-import { NextResponse } from "next/server";
-
-export async function GET(request: Request) {
-   await connectToDatabase();
-
-   return NextResponse.json({ message: "Hello, Next.js!" });
-}
