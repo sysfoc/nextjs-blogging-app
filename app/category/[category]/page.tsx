3d271dd1911aea850504261ad3d2db986e6633f8
hamza-fullstackdev <hamzafullstackdev1@gmail.com>
2025-09-04 09:03:56 +0500

    Minor Fixes

--- app/category/[category]/page.tsx
+++ app/category/[category]/page.tsx
@@ -1,13 +1,22 @@
 import Category from "@/app/(public)/pages/Category";
 import { Metadata } from "next";
 type Props = {
-  params: { category: string };
+  params: Promise<{
+    category: string;
+  }>;
 };
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { category } = await params;
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/category/get-by-category/${params.category}`,
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/category/get-by-category/${category}`,
     { cache: "no-store" }
   );
+  if (!res.ok) {
+    return {
+      title: "Category does not exist",
+      description: "Category does not exist",
+    };
+  }
   const data = await res.json();
   return {
     title: data?.category?.metaTitle || "Category does not exist",
