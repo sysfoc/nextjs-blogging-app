a6b3c9d4f70684218a42a5320fcb7e700d2a8d24
SYSFOC <143105442+sysfoc@users.noreply.github.com>
2025-12-05 12:08:32 +0500

    cloudflare deploy  test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: false, 
+  },
 };
 
 export default nextConfig;
