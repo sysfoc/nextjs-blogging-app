8acd98655758feec6da1d2d517428656c19394ad
Rehan Ahmad <rehan048686@gmail.com>
2025-12-19 15:42:32 +0500

    nextjs configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    unoptimized: true,
+    domains: ['networthmama.com', 'www.networthmama.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'networthmama.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.networthmama.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
