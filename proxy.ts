e584e50197c4bca110609649cdae9662a6209a6b
Rehan Ahmad <rehan048686@gmail.com>
2025-12-22 10:00:59 +0500

    nextjs middleware file rename

--- proxy.ts
+++ proxy.ts
@@ -14,7 +14,7 @@ async function verifyToken(token: string) {
   }
 }
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
   if (pathname.startsWith("/admin")) {
